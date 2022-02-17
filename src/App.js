3d72eedf885fa1b1366a0c6f88b125ab8ab68ee2
hasib <hansari@appstec-me.com>
2022-02-17 16:44:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Error from './components/Error';
-import {BrowserRouter} from 'react-router-dom';
+
 // import {Routes} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
 import Sidebar from './components/Sidebar';
@@ -14,19 +14,17 @@ import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
     <div >
-      <BrowserRouter>
 
-        <Routes>
-          <Route path='/' index element={<Login />} />
-          {/* <Route element={<ProtectedRoute />}> */}
-          <Route path='/navbar' index element={<Navbar />} />
+      <Routes>
+        <Route path='/' index element={<Login />} />
+        {/* <Route element={<ProtectedRoute />}> */}
+        <Route path='/navbar' index element={<Navbar />} />
 
-          <Route path='/feed' index element={<Feed />} />
-          {/* </Route> */}
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Route path='/feed' index element={<Feed />} />
+        {/* </Route> */}
+        <Route path="*" element={<Error />} />
+      </Routes>
 
-      </BrowserRouter>
     </div>
   );
 }
